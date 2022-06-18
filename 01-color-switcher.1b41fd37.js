974f6dc3c79f2552333bd2969d18dffcc1647ecd
Osyotr1 <92748388+Osyotr1@users.noreply.github.com>
2022-06-18 14:43:53 +0000

    Deploying to gh-pages from @ Osyotr1/goit-js-hw-09@eaeb512fb646e39df6120a0b49bbf68356965696 🚀

---
+++ 01-color-switcher.1b41fd37.js
@@ -0,0 +1,2 @@
+!function(){var t=null,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");n.disabled=!0,e.addEventListener("click",(function(){e.disabled=!0,n.disabled=!1,t=setInterval((function(){document.body.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16)))}),1e3),console.log(e)})),n.addEventListener("click",(function(){n.disabled=!0,e.disabled=!1,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.1b41fd37.js.map
