eaeb512fb646e39df6120a0b49bbf68356965696
Volodymyr Horin <vova.gorin08@gmail.com>
2022-06-18 17:43:24 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,44 @@
 
+
+let timerId = null;
+
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+
+btnStop.disabled = true;
+
+btnStart.addEventListener('click', () => {
+    disabeStartBtn();   
+    timerId = setInterval(() => {
+      document.body.style.backgroundColor = `${getRandomHexColor()}`
+    }, 1000);
+    console.log(btnStart);
+});
+
+btnStop.addEventListener('click', () => {
+    disableStopBtn(); 
+    clearInterval(timerId);
+});
+
+
+function disabeStartBtn() {
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+}
+
+function disableStopBtn() {
+    btnStop.disabled = true;
+    btnStart.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+
+
+
+
+
