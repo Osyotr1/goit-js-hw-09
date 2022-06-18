89626a823cfeb331a8db547c5554bc6351f098f2
Osyotr1 <92748388+Osyotr1@users.noreply.github.com>
2022-06-18 15:48:07 +0000

    Deploying to gh-pages from @ Osyotr1/goit-js-hw-09@381985b665a674405c01b9229bab7ab2ae3887a4 🚀

--- 01-color-switcher.1fdd8f5e.js
+++
@@ -1,2 +0,0 @@
-let e=null;const t=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");d.disabled=!0,t.addEventListener("click",(()=>{t.disabled=!0,d.disabled=!1,e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),console.log(t)})),d.addEventListener("click",(()=>{d.disabled=!0,t.disabled=!1,clearInterval(e)}));
-//# sourceMappingURL=01-color-switcher.1fdd8f5e.js.map
